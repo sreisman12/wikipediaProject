6a5ed8b119ede0a84405c795e8beeb3adfd644b4
Steven Reisman <steven.reisman@philips.com>
2020-02-15 20:15:19 -0500

    Updating param type of getResources interface

--- java/src/main/java/com/viome/dao/IWikiDAO.java
+++ java/src/main/java/com/viome/dao/IWikiDAO.java
@@ -1,14 +1,14 @@
 package com.viome.dao;
 
-import org.omg.CORBA.NameValuePair;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public interface IWikiDAO<Resource> {
-    public List<Resource> getResources(List<NameValuePair> searchParams);
+    public List<Resource> getResources(Map<String,String> searchParams);
     public Resource getResourceById(String id) throws SQLException;
 
 }
