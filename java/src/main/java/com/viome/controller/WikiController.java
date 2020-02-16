5c7f872afad44336b785a1e056d4204a12cff3e0
Steven Reisman <steven.reisman@philips.com>
2020-02-15 22:52:49 -0500

    Implementing fetch resources by search map function

--- java/src/main/java/com/viome/controller/WikiController.java
+++ java/src/main/java/com/viome/controller/WikiController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 @RestController
 public class WikiController {
@@ -20,6 +21,12 @@ public class WikiController {
     public ResponseEntity<String> getWikiPageById(@PathVariable String id) throws SQLException {
         Resource resource = (Resource) wikiDao.getResourceById(id);
         return new ResponseEntity<>("Fetching wiki page with id: " + id, HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(value = "/wikiPage", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getResources(@RequestParam Map<String, String> searchParams) throws SQLException {
+        Resource resource = (Resource) wikiDao.getResources(searchParams);
+        return new ResponseEntity<>("Fetching wiki page with id: ",  HttpStatus.ACCEPTED);
     }
 
 
