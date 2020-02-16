7049734e7d1fc70f5d5437de3ad89d46ef9730aa
Ubuntu <ubuntu@ip-172-31-19-197.us-west-2.compute.internal>
2020-02-16 18:34:08 +0000

    Cleaning up wikiFetcher script

--- utils/wikiFetcher.py
+++ utils/wikiFetcher.py
@@ -24,21 +24,34 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 count = int(sys.argv[1])
+language = str(sys.argv[2])
 print(str(sys.argv[1]))
+
+if (language is None):
+	language = "en"
+
+print("Using language: " + language)
+
+# Set language for articles from passed parameter
+wikipedia.set_lang(language)
 
 while count < 5000:
 	try:
 		content = wikipedia.WikipediaPage(title=wikipedia.random())
 	except wikipedia.DisambiguationError, err:
 		#pdb.set_trace()
-		content = wikipedia.page(err.options[0])
+		#Nested try for issues which seem to come up with mutlipe disambiugation errors
+		try: 
+			content = wikipedia.page(err.options[0])
+		except wikipedia.DisambiguationError, err:
+			content = wikipedia.page(err.options[0])
 	try:
 		title = content.title
 	except wikipedia.DisambiguationError, err:
 		pdb.set_trace()
 		title = err.options[0]
-	sql = "INSERT INTO page (title, body, url) VALUES (%s, %s, %s)"
-	val = (title, content.content.encode(encoding='UTF-8', errors='strict'), content.url)
+	sql = "INSERT INTO page (title, body, url, language) VALUES (%s, %s, %s, %s)"
+	val = (title, content.content.encode(encoding='UTF-8', errors='strict'), content.url, language)
 	print("Inserting page: " + content.title)
 	try:
 		mycursor.execute(sql, val)
