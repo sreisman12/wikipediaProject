e08375a296f3b27aa30f30a5fe8d4c619c5e54ab
Steven Reisman <steven.reisman@philips.com>
2020-02-16 18:47:09 -0500

    Adding route to support query params for flask app

--- python/pythonWikiApp.py
+++ python/pythonWikiApp.py
@@ -2,42 +2,45 @@ from flask import Flask, request, jsonify
 import mysql.connector
 
 mysqlDb = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	passwd="root",
-	database="wiki",
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="wiki",
 )
 
 dbCursor = mysqlDb.cursor()
 
 app = Flask(__name__)
 
+
 @app.route("/wikiPage/<id>")
 def fetchPageById(id):
-	dbCursor.execute("SELECT * FROM page WHERE id=" + id + ";")
-	wikiPage = dbCursor.fetchone()
-	return jsonify(wikiPage)
+    dbCursor.execute("SELECT * FROM page WHERE id=" + id + ";")
+    wikiPage = dbCursor.fetchone()
+    return jsonify(wikiPage)
+
 
 @app.route("/wikiPage")
 def fetchPagesByParams():
-	searchKeys = request.args.keys()
-	query = "SELECT * FROM page WHERE "
-	
-	paramsList = []
-	for key in searchKeys:
-		if(key is not "reverse"):
-			paramsList.append(query + key + "  LIKE '%" + str(searchKeys[key] + "%'")
+    searchKeys = request.args.keys()
+    query = "SELECT * FROM page WHERE "
 
-	for i, param in paramsList:
-	    if i != len(paramsList) - 1:
-	        query = query + param + " AND "
-	    else:
-	        query = query + param
-	query = query + ";"
+    paramsList = []
+    for key in searchKeys:
+        if key is not "reverse":
+            paramsList.append(query + key + "  LIKE '%" + str(searchKeys[key] + "%'"))
 
-	dbCursor.execute(query)
-	wikiPages = dbCursor.fetchall()
-	return jsonify(wikiPages)
+        for i, param in paramsList:
+            if i != len(paramsList) - 1:
+                query = query + param + " AND "
+            else:
+                query = query + param
+            query = query + ";"
 
-if __name__=="__main__":
-	app.run(debug=True)
+            dbCursor.execute(query)
+            wikiPages = dbCursor.fetchall()
+    return jsonify(wikiPages)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
