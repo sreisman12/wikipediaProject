85c088fdcece2fd14c94067b274ce62406d827b5
Steven Reisman <steven.reisman@philips.com>
2020-02-16 16:05:20 -0500

    Adding basic functionality to mongo insertion script

---
+++ utils/mongoInsertion.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from pymongo import MongoClient
+import mysql.connector
+import random
+import pdb
+
+MONGO_HOST='localhost'
+MONGO_PORT=27017
+MONGO_DB='content'
+MONGO_COLLECTION='pages'
+
+MYSQL_HOST='localhost'
+MYSQL_DB='wiki'
+MYSQL_USER='root'
+MYSQL_PASS='root'
+
+mongoConnection = MongoClient(MONGO_HOST, MONGO_PORT)
+wikiCollection = mongoConnection[MONGO_DB][MONGO_COLLECTION]
+
+mysqlConnection = mysql.connector.connect(
+    host = MYSQL_HOST,
+    user = MYSQL_USER,
+    passwd = MYSQL_PASS,
+    database = MYSQL_DB
+)
+
+mysqlCursor = mysqlConnection.cursor()
+
+#choose random record from between the 10000 stored wikipages
+randomPageId = random.randint(1,10000)
+sql = "SELECT title, body, url FROM page WHERE id =" + str(randomPageId)
+wikiPage = mysqlCursor.execute(sql)
+pdb.set_trace()
+
+
+
